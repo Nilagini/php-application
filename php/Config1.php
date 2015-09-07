5d58d4052d9c3f5c96ee15e032b0026acdec3e0f
Nilasini <nilathiru93@gmail.com>
2015-09-08 00:11:48 +0530

    adding folder

---
+++ php/Config1.php
@@ -0,0 +1,7 @@
+<?php
+ // Database configuration
+    define('DB_USERNAME', 'a4516290_org');
+    define('DB_PASSWORD', 'org123');
+    define('DB_HOST', 'http://mysql8.000webhost.com/');
+    define('DB_NAME', 'a4516290_org');
+?>
